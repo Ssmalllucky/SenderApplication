bbf7c667e1a4432f7755dbf3709ac31bd5a41972
ssmalllucky <ssmalllucky@163.com>
2020-04-27 23:58:16 +0800

    Initial commit

---
+++ app/src/main/java/com/qwer/senderapplication/SenderActivity.java
@@ -0,0 +1,40 @@
+package com.qwer.senderapplication;
+
+import android.content.Intent;
+import android.database.ContentObserver;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SenderActivity extends AppCompatActivity implements Switch.OnCheckedChangeListener {
+
+    private Switch mSwSender;
+    public static final String PREFERENCE_NAME = "receiver_settings";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aty_sender);
+
+        mSwSender = findViewById(R.id.sw_sender);
+        mSwSender.setOnCheckedChangeListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        Intent intent = new Intent("demo.action.RECEIVED_SWITCH_VALUE");
+        intent.setPackage("com.qwer.receiverapplication");
+        intent.putExtra("key_switch_status", isChecked ? 1 : 0);
+        sendBroadcast(intent);
+
+    }
+}
